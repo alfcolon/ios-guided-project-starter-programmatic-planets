3e02ade4faae77c76b58fb7667f678662261a9bd
alfcolon <alfcolon@gmail.com>
2020-01-28 13:00:58 -0800

    Completed project

--- ProgrammaticPlanets/View Controllers/SettingsViewController.swift
+++ ProgrammaticPlanets/View Controllers/SettingsViewController.swift
@@ -15,16 +15,102 @@ class SettingsViewController: UIViewController {
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
+        setUpSubviews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateViews()
     }
-    
+
+    func setUpSubviews(){
+        //MARK: Create Button
+        
+        let doneButton = UIButton(type: .system)
+        
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.setTitle("Done", for: .normal)
+        //target: class where button is run
+        //action: pass method to be run when action is triggered
+        //for: type of event that will trigger the action
+        doneButton.addTarget(self, action: #selector(done), for: .touchUpInside)
+        
+        //MARK: Add Button to Subview
+        
+        self.view.addSubview(doneButton)
+        
+        //MARK: Add Constraints
+        
+        let doneButtonTopConstraint = NSLayoutConstraint(item: doneButton,
+                                                         attribute: .top,
+                                                         relatedBy: .equal,
+                                                         toItem: view.safeAreaLayoutGuide,
+                                                         attribute: .top,
+                                                         multiplier: 1,
+                                                         constant: 20)
+        let doneButtonTrailingConstraint = NSLayoutConstraint(item: doneButton,
+                                                              attribute: .trailing,
+                                                              relatedBy: .equal,
+                                                              toItem: view.safeAreaLayoutGuide,
+                                                              attribute: .trailing,
+                                                              multiplier: 1,
+                                                              constant: -20)
+        
+        //MARK: Activate Constraints
+        
+        NSLayoutConstraint.activate([doneButtonTopConstraint, doneButtonTrailingConstraint])
+        
+        //MARK: Create Switch
+            
+        let shouldShowPlutoSwitch = UISwitch()
+        
+        //MARK: Turn Autoresizing Off
+        
+        shouldShowPlutoSwitch.translatesAutoresizingMaskIntoConstraints = false
+        
+        //MARK: Add Target
+        
+        shouldShowPlutoSwitch.addTarget(self, action: #selector(changeShouldShowPluto(_:)), for: .valueChanged)
+        
+        //MARK: Add Switch to Subview
+        
+        self.view.addSubview(shouldShowPlutoSwitch)
+        
+        //MARK: Add Constraints
+        
+        let switchTopConstraint = shouldShowPlutoSwitch.topAnchor.constraint(equalTo: doneButton.bottomAnchor, constant: 60)
+        let switchTrailingConstraint = shouldShowPlutoSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        //MARK: Activate Constraints
+        
+        switchTopConstraint.isActive = true
+        switchTrailingConstraint.isActive = true
+        
+        
+        //MARK: Assign Switch Value to Switch Property
+        
+        self.shouldShowPlutoSwitch = shouldShowPlutoSwitch
+        
+        //Label
+        
+        //MARK: Create Label
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.text = "Is Pluto a planet?"
+        
+        view.addSubview(label)
+        
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        label.trailingAnchor.constraint(equalTo: shouldShowPlutoSwitch.leadingAnchor, constant: -8).isActive = true
+        label.centerYAnchor.constraint(equalTo: shouldShowPlutoSwitch.centerYAnchor).isActive = true
+        
+        
+    }
     // MARK: - Action Handlers
     
-    func changeShouldShowPluto(_ sender: UISwitch) {
+    @objc func changeShouldShowPluto(_ sender: UISwitch) {
         let userDefaults = UserDefaults.standard
         userDefaults.set(sender.isOn, forKey: .shouldShowPlutoKey)
     }
@@ -37,6 +123,6 @@ class SettingsViewController: UIViewController {
     
     private func updateViews() {
         let userDefaults = UserDefaults.standard
-        shouldShowPlutoSwitch.isOn = userDefaults.bool(forKey: .shouldShowPlutoKey)
+//        shouldShowPlutoSwitch.isOn = userDefaults.bool(forKey: .shouldShowPlutoKey)
     }
 }
--- ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
+++ ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
@@ -18,7 +18,70 @@ class PlanetCollectionViewCell: UICollectionViewCell {
             updateViews()
         }
     }
+    //Used when initiating a new cell, etc. programmatically
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubiews()
+    }
+    //Used by the Storyboard when initiating a new cell, etc. programmatically
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubiews()
+    }
     
+    func setUpSubiews(){
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        
+        //Constraints
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+            constant: 4).isActive = true
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+            constant: -4).isActive = true
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        //Connect local constant to property
+        self.imageView = imageView
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        addSubview(label)
+        
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
+        
+        self.nameLabel = label
+        
+        
+    }
     private func updateViews() {
         imageView.image = planet.image
         nameLabel.text = planet.name
